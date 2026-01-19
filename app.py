e12ef53555bdb3557404c62c67eb56dfdf447fb9
Athul Titus <titusathul8@gmail.com>
2026-01-19 11:11:42 +0530

    connection modified

--- app.py
+++ app.py
@@ -228,4 +228,4 @@ def fetch_from_peer():
 
 if __name__ == '__main__':
     print("Starting Antigravity Quantum Server...")
-    app.run(port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
